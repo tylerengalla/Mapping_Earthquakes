769b7b741b05887a98cef399ade1f5b80012b5fb
tylerengalla <71526893+tylerengalla@users.noreply.github.com>
2021-01-16 22:58:13 -0600

    Mult Points

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -7,17 +7,32 @@ console.log("working");
 // Alt to above code: Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
-      34.0522, -118.2437
+      40.7, -94.5
     ],
-    zoom: 14
+    zoom: 4
   });
 
-// Add a marker to the map for Los Angeles, California.
-let marker = L.circleMarker([34.0522, -118.2437], {
-  color: 'black',
-  fillColor: '#ffffa1',
-  radius: 300
-}).addTo(map);
+// Add a single marker to the map for Los Angeles, California.
+// let marker = L.circleMarker([34.0522, -118.2437], {
+//   color: 'black',
+//   fillColor: '#ffffa1',
+//   radius: 300
+// }).addTo(map);
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one maker for each city
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius: city.population/200000,
+    color: 'orange',
+    weight: 4
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population: " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
 
 
 // We create the tile layer that will be the background of our map.
@@ -26,6 +41,9 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
         maxZoom: 18,
         accessToken: API_KEY
     });
+
+    // day light api
+    // 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}'
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
